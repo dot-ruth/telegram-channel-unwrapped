5f7d749b528a4f47d32a9ed505aa5c00265a0925
dot-ruth <ruthgetaneh5@gmail.com>
2025-12-07 08:03:55 +0300

    fix: telethon session persistance

--- telegram_client.py
+++ telegram_client.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 
 load_dotenv()
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
+SESSION_STRING = os.getenv("SESSION_STRING")
 
-client = TelegramClient("unwrapped_session", API_ID, API_HASH)
+client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
